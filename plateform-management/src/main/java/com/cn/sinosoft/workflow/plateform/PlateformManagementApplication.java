34364a3c862f50c075ba66895cfb49a3b1e473a2
17695999671 <1239010977@qq.com>
2018-10-26 18:03:41 +0800

    add palteform-manager

---
+++ plateform-management/src/main/java/com/cn/sinosoft/workflow/plateform/PlateformManagementApplication.java
@@ -0,0 +1,12 @@
+package com.cn.sinosoft.workflow.plateform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformManagementApplication.class, args);
+	}
+}
---
+++ plateform-management/src/test/java/com/cn/sinosoft/workflow/plateform/PlateformManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cn.sinosoft.workflow.plateform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlateformManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
